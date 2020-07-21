e976faa7a3afacf13668534688cc396326371bb7
ivan1120 <chenivan5@gmail.com>
2020-07-21 11:44:03 -0700

    fix deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
                 <Route path="/resume">
                   <ResumePage/>
                 </Route>
-                <Route path="/">
+                <Route>
                   <ResumePage/>
                 </Route>
             </Switch>
