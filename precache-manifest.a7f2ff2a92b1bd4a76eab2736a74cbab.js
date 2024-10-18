231eab164bd5e2a9675f362466f2466e61e2baf2
Ivan Chen <chenivan5@gmail.com>
2024-10-18 00:22:03 -0700

    Updates

---
+++ precache-manifest.a7f2ff2a92b1bd4a76eab2736a74cbab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7b5f18006e5e314458110f13794b66",
+    "url": "/personal_website2/index.html"
+  },
+  {
+    "revision": "a1a0222f89e654ffb209",
+    "url": "/personal_website2/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "2862f69f4a2eee5e4444",
+    "url": "/personal_website2/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "a1a0222f89e654ffb209",
+    "url": "/personal_website2/static/js/2.29d16d30.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/personal_website2/static/js/2.29d16d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2862f69f4a2eee5e4444",
+    "url": "/personal_website2/static/js/main.3bee77d6.chunk.js"
+  },
+  {
+    "revision": "3e80eeb9f005a7c1c50b",
+    "url": "/personal_website2/static/js/runtime-main.62a6b823.js"
+  },
+  {
+    "revision": "0916c14c657a044ef3319fb67985a968",
+    "url": "/personal_website2/static/media/myPic.0916c14c.PNG"
+  }
+]);
