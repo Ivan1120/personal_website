276b92a8a02a8ce8d93b41b7fc1e7996442dea25
Ivan Chen <chenivan5@gmail.com>
2024-10-20 21:36:04 -0700

    fix package.json script

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -15,19 +15,6 @@ const Experiences = [
             'Collaborated with stakeholders to meet product requirements and develop 10+ POCs',
             'Resolved 10+ production issues with clients, ensuring system reliability and high client satisfaction'
         ]
-    },
-    {
-        jobTitle: 'Web development Intern ',
-        note: '',
-        company: 'RoRo Holding Inc',
-        location: 'Vancouver, BC, Canada',
-        startDate: new Date(2019, 4),
-        endDate: new Date(2019,7),
-        description: [
-            'Created front-end pages utilizing ReactJS and Bootstrap, ensuring seamless integration with the back-end API',
-            'Implemented RESTful endpoints using Spring Boot, integrated with PostgreSQL for database management',
-            'Applied responsive design techniques to ensure compatibility and optimal user experience across various devices'
-        ]
     }
 
   
