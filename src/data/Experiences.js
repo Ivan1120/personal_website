74474da22e2d46e5dd5cf7844315a0537544ad53
Ivan Chen <chenivan5@gmail.com>
2024-12-01 10:08:33 -0800

    minor change

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -1,27 +1,28 @@
 const Experiences = [
-    {
-        jobTitle: 'Software Developer ',
-        note: '',
-        company: 'HigherStack Software',
-        location: '',
-        startDate: new Date(2023, 4),
-        endDate: new Date(),
-        description: [
-            'Architected and developed scalable software solutions for a variety of projects using ASP.NET, PHP, and Node.js',
-            'Developed database schemas, internal APIs, and business logic using ASP.NET and C#',
-            'Optimized SQL Server query performance by enhancing statements, achieving a 30% improvement',
-            'Developed and optimized dynamic frontend components using React.js to enhance user experience',
-            'Identified and resolved coding issues, ensuring optimal software performance and functionality'
-        ]
-    }
-   ,
+//     {
+//         jobTitle: 'Software Developer ',
+//         note: '',
+//         company: 'HigherStack Software',
+//         location: '',
+//         startDate: new Date(2023, 4),
+//         endDate: new Date(),
+//         description: [
+//             'Architected and developed scalable software solutions for a variety of projects using ASP.NET, PHP, and Node.js',
+//             'Developed database schemas, internal APIs, and business logic using ASP.NET and C#',
+//             'Optimized SQL Server query performance by enhancing statements, achieving a 30% improvement',
+//             'Developed and optimized dynamic frontend components using React.js to enhance user experience',
+//             'Identified and resolved coding issues, ensuring optimal software performance and functionality'
+//         ]
+//     }
+//    ,
     {
         jobTitle: 'Software Developer ',
         note: '',
         company: 'D3 Security Management Systems',
         location: '',
         startDate: new Date(2020, 10),
-        endDate: new Date(2023,3),
+        // endDate: new Date(2023,3),
+        endDate: new Date(),
         description: [
             'Designed and maintained integrations of the company\'s core product with 100+ external applications and APIs using C# and ASP.NET',
             'Developed 10+ database schemas, internal APIs, and business logic to process integration requests',
