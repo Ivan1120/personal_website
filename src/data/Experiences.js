a61f64dfa9bc8147d8ee2e9521544b38e395092e
Ivan Chen <chenivan5@gmail.com>
2024-12-01 22:14:15 -0800

    minor change

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -9,9 +9,10 @@ const Experiences = [
         // endDate: new Date(2023,3),
         endDate: new Date(),
         description: [
-            'Designed and maintained integrations of the company\'s core product with 100+ external applications and APIs using C# and ASP.NET',
             'Developed database schemas, internal APIs, and business logic to process integration requests',
             'Developed and optimized dynamic frontend components using React.js to enhance user experience',
+            'Designed and maintained integrations of the company\'s core product with 100+ external applications and APIs',
+
             'Collaborated with stakeholders to meet product requirements and develop 10+ POCs',
             'Resolved production issues with clients, ensuring system reliability and high client satisfaction'
         ]
