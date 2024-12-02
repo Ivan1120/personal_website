bef75b1733c6b37dccd6ab3c5e16adff17266ae8
Ivan Chen <chenivan5@gmail.com>
2024-12-01 20:37:34 -0800

    minor change

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -1,20 +1,5 @@
 const Experiences = [
-//     {
-//         jobTitle: 'Software Developer ',
-//         note: '',
-//         company: 'HigherStack Software',
-//         location: '',
-//         startDate: new Date(2023, 4),
-//         endDate: new Date(),
-//         description: [
-//             'Architected and developed scalable software solutions for a variety of projects using ASP.NET, PHP, and Node.js',
-//             'Developed database schemas, internal APIs, and business logic using ASP.NET and C#',
-//             'Optimized SQL Server query performance by enhancing statements, achieving a 30% improvement',
-//             'Developed and optimized dynamic frontend components using React.js to enhance user experience',
-//             'Identified and resolved coding issues, ensuring optimal software performance and functionality'
-//         ]
-//     }
-//    ,
+
     {
         jobTitle: 'Software Developer ',
         note: '',
@@ -25,10 +10,10 @@ const Experiences = [
         endDate: new Date(),
         description: [
             'Designed and maintained integrations of the company\'s core product with 100+ external applications and APIs using C# and ASP.NET',
-            'Developed 10+ database schemas, internal APIs, and business logic to process integration requests',
-            'Developed and optimized 10+ dynamic frontend components using React.js to enhance user experience',
+            'Developed database schemas, internal APIs, and business logic to process integration requests',
+            'Developed and optimized dynamic frontend components using React.js to enhance user experience',
             'Collaborated with stakeholders to meet product requirements and develop 10+ POCs',
-            'Resolved 10+ production issues with clients, ensuring system reliability and high client satisfaction'
+            'Resolved production issues with clients, ensuring system reliability and high client satisfaction'
         ]
     }
 
